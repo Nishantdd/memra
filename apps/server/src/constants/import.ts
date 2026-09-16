d55d20c5fdbbcfa197ec3486f7876f1b12befa6a
Nishantdd <nishant.dahiya2000@gmail.com>
2026-09-16 10:07:52 +0000

    feat(server): add import constants

---
+++ apps/server/src/constants/import.ts
@@ -0,0 +1,2 @@
+export const MAX_REQUEST_BYTES = 52 * 1024 * 1024;
+export const EXPORT_ZIP_NAME = "memra-export";
--- apps/server/src/constants/index.ts
+++ apps/server/src/constants/index.ts
@@ -2,6 +2,7 @@ export * from "./app.ts";
 export * from "./ask.ts";
 export * from "./auth.ts";
 export * from "./http.ts";
+export * from "./import.ts";
 export * from "./profiles.ts";
 export * from "./rag.ts";
 export * from "./search.ts";
